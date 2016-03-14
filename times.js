7a4856d2addbc54bbaa99381a1c4ba8fd7329078
scotteggs <scotteggs@gmail.com>
2016-03-14 16:47:53 -0400

    re-order filed output to match database needs

--- times.js
+++ times.js
@@ -26,6 +26,7 @@ fs.readFile("./csvInput/pfwtimes.csv", 'utf8', function (err,data) {
 		var currentTaskOrder;
 		var currentProjectActive;
 		var count = 0;
+		var newObject;
 
 
 		fs.createReadStream('./csvInput/pfwtimes_noQuotes.csv')
@@ -59,22 +60,26 @@ fs.readFile("./csvInput/pfwtimes.csv", 'utf8', function (err,data) {
 		      data.TASK_PHASE = currentTaskPhase;
 		      data.TASK_ORDER = currentTaskOrder;
 		      data.TASK_TITLE = currentTaskTitle;
-		      data.TASKID = data['JOB_ID'] + " | " + data['JOB_NAME'] + " | " + data['JOB_TITLE'] + " | " + data['TASK_ORDER'] + " | " + data['TASK_PHASE'] + " | " + data['TASK_TITLE'];
-		      delete data['PROJECT CATEGORY'];
-		      delete data['JOBNUMBER'];
-		      delete data['JOBID'];
-		      delete data['JOBCOMPLETE'];
-		      delete data[''];
-		      delete data['ACTIVE  COMPLETED'];
-		      delete data['TASKORDER'];
-		      delete data['TASKTITLE'];
-		      delete data['ROW TYPE'];
-		      delete data['JOBTITLE'];
-		      delete data['TASKASSIGNEDTO'];
-		      delete data['TASKPRIORITY'];
+		      data['TASKID'] = data['JOB_ID'] + " | " + data['JOB_NAME'] + " | " + data['JOB_TITLE'] + " | " + data['TASK_ORDER'] + " | " + data['TASK_PHASE'] + " | " + data['TASK_TITLE'];
+
 		      //actions for adding time duration field
 		      data.DURATION = moment.duration(data['TRACKERTIMESPENT']).asHours();
-		      output.push(data);
+		      
+		      //creation of newObject to re-order output
+		      newObject = {};
+		      newObject.KEY = data['TASKID'] + " | " + data['TRACKERUSER'];
+		      newObject.USER = data['TRACKERUSER'];
+		      newObject.TRANSACTION_DATE = data['TRACKERDATE'];
+		      newObject.TIME_SPENT = data['TRACKERTIMESPENT'];
+		      newObject.DURATION = data.DURATION;
+		      newObject.NOTES = data['TRACKERNOTES'];
+		      newObject.JOB_ID = currentJobID;
+		      newObject.JOB_NAME = currentJobName;
+		      newObject.JOB_TITLE = currentJobTitle;
+		      newObject.TASK_PHASE = currentTaskPhase;
+		      newObject.TASK_ORDER = currentTaskOrder;
+		      newObject.TASK_TITLE = currentTaskTitle;
+		      output.push(newObject);
 		      count += 1;
 		    }
 		    // output.push(data);
@@ -86,9 +91,9 @@ fs.readFile("./csvInput/pfwtimes.csv", 'utf8', function (err,data) {
 
 
 		    function compare(a,b) {
-  				if (a.TASKID < b.TASKID)
+  				if (a.KEY < b.KEY)
     				return -1;
-  			else if (a.TASKID > b.TASKID)
+  			else if (a.KEY > b.KEY)
     			return 1;
   			else 
     			return 0;
@@ -96,7 +101,7 @@ fs.readFile("./csvInput/pfwtimes.csv", 'utf8', function (err,data) {
 
 				output.sort(compare);
 
-		  	for (let i = 5000; i < 5500; i++) {
+		  	for (let i = 5400; i < 5500; i++) {
 		  			console.log(output[i]);
 		  	}
 		  	console.log(output[output.length-1]);
