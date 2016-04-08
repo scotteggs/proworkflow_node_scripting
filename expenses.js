ec378f322d6a0b812e90980e162ea13496a9ee03
scotteggs <scotteggs@gmail.com>
2016-04-08 16:40:35 -0400

    updates code comments

--- expenses.js
+++ expenses.js
@@ -1,68 +1,67 @@
 "use strict";
 
 var fs = require('fs');
+var csv = require('csv-parser');
+var fastcsv = require('fast-csv');
+var moment = require('moment');
 
 fs.readFile("./csvInput/expenses_download.csv", 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
+  //add additional header to capture Amount data
   data = data.replace(/,Amount/g, ',Amount,Amount2');
+  //cut off header from data and save to new file
   var result = data.slice(data.search("Date"));
   fs.writeFile("./csvInput/project_expenses.csv", result, 'utf8', function (err,data) {
   	if (err) return console.log(err);
-  	var csv = require('csv-parser');
-		var fastcsv = require('fast-csv');
-		var fs = require('fs');
-		var moment = require('moment');
-
+  	//setup csv stream properties
 		var stream = csv({
 		  separator: ',',
 		  headers: false
-
 		})
-		var output2 = [];
+		//create blank array to capture output
 		var output = [];
 		var count = 0; 
 		var positionStart;
 		var positionEnd;
 
-
 		fs.createReadStream('./csvInput/project_expenses.csv')
-		  .pipe(stream)
-		  .on('data', function(data) {
-			  var newObj = {};
-				if (data['Date'] == '' && data['Client'] != '') {
-					positionStart = data['Account'].indexOf(':');
-					positionEnd = data['Account'].indexOf(' ',positionStart);
-					data['Project'] = data['Account'].slice(positionStart+1,positionEnd);
-					//create unique identifier from project number and invoice number ('Name')
-					newObj['Key'] = data['Project'] + "_" + data['Name'];
-					//create remaining fields
-					newObj['Project'] = data['Project'];
-					newObj['Invoice'] = data['Name'];
-					newObj['Date'] = data['Client'];
-					newObj['Vendor'] = data['Memo/Description'];
-					newObj['Amount'] = parseFloat(data['Amount2'].replace(',',''));
-					newObj['Client'] = data['Account'];
-					newObj['Memo'] = data['Amount'];
-					output.push(newObj);
-					count ++;
-				}
-				
+	  .pipe(stream)
+	  .on('data', function(data) {
+		  var newObj = {};
+			if (data['Date'] == '' && data['Client'] != '') {
+				positionStart = data['Account'].indexOf(':');
+				positionEnd = data['Account'].indexOf(' ',positionStart);
+				data['Project'] = data['Account'].slice(positionStart+1,positionEnd);
+				//create unique identifier from project number and invoice number ('Name')
+				newObj['Key'] = data['Project'] + "_" + data['Name'];
+				//create remaining fields
+				newObj['Project'] = data['Project'];
+				newObj['Invoice'] = data['Name'];
+				newObj['Date'] = data['Client'];
+				newObj['Vendor'] = data['Memo/Description'];
+				newObj['Amount'] = parseFloat(data['Amount2'].replace(',',''));
+				newObj['Client'] = data['Account'];
+				newObj['Memo'] = data['Amount'];
+				output.push(newObj);
+				count ++;
+			}
+	  })
 
-		  })
-
-
-		  .on('end', function () {
-		  	console.log(output[0])
-		  	console.log(output[output.length-1])
-		  	console.log("count is ", count)
-		    var outputStream = fastcsv.createWriteStream({headers: true});
-		    var writableStream = fs.createWriteStream("./csvOutput/expensesOutput.csv");
-		    outputStream.pipe(writableStream);
-		    for (var row in output) {
-		      outputStream.write(output[row]);
-		    }
-		  })
-		})
+	  .on('end', function () {
+	  	//print first and last object to console along with count
+	  	console.log(output[0])
+	  	console.log(output[output.length-1])
+	  	console.log("count is ", count)
+	  	//create output data stream using fast-csv library
+	    var outputStream = fastcsv.createWriteStream({headers: true});
+	    //declare path for final output file
+	    var writableStream = fs.createWriteStream("./csvOutput/expensesOutput.csv");
+	    outputStream.pipe(writableStream);
+	    for (var row in output) {
+	      outputStream.write(output[row]);
+	    }
+	  })
+	})
 })
